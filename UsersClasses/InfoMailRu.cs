4eef22a3067de2194dad297390df7c0a6c8da5a3
Admin <Admin@DESKTOP-MAHVHFA>
2026-03-02 15:52:10 +0300

    InfoMailRu

---
+++ UsersClasses/InfoMailRu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR5_s8.UsersClasses
+{
+    internal class InfoMailRu : InfoEmail
+    {
+        public InfoMailRu(StringPair emailAdressTo, string subject, string body) : base(emailAdressTo, subject, body)
+        {
+            SmtpClientAdress = "smtp.mail.ru";
+            EmailAdressFrom = new StringPair("olezasinnicyn@mail.ru", "Вещий Олег");
+            EmailPassword = "bosOvHLbCBVs0i4ZeDI5";
+            Port = -1;
+        }
+    }
+}
