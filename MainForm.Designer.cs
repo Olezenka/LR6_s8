8da988ff9c3467fa2828a02da2d4327bcdce7ed5
Admin <Admin@DESKTOP-MAHVHFA>
2026-03-02 16:28:28 +0300

    внешние корректировки

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -48,12 +48,11 @@
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe Print", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(278, 35);
+            this.label1.Location = new System.Drawing.Point(12, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(540, 65);
+            this.label1.Size = new System.Drawing.Size(1171, 65);
             this.label1.TabIndex = 0;
             this.label1.Text = "Отправление email писем";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -64,7 +63,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(48, 126);
+            this.label2.Location = new System.Drawing.Point(48, 133);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(130, 56);
             this.label2.TabIndex = 1;
@@ -77,7 +76,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label3.Location = new System.Drawing.Point(48, 209);
+            this.label3.Location = new System.Drawing.Point(48, 216);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(126, 56);
             this.label3.TabIndex = 2;
@@ -90,7 +89,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label4.Location = new System.Drawing.Point(643, 209);
+            this.label4.Location = new System.Drawing.Point(643, 216);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(109, 56);
             this.label4.TabIndex = 3;
@@ -103,7 +102,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label5.Location = new System.Drawing.Point(48, 296);
+            this.label5.Location = new System.Drawing.Point(48, 303);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(187, 56);
             this.label5.TabIndex = 4;
@@ -116,7 +115,7 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label6.Location = new System.Drawing.Point(48, 370);
+            this.label6.Location = new System.Drawing.Point(48, 377);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(130, 56);
             this.label6.TabIndex = 5;
@@ -125,9 +124,9 @@
             // 
             // textBoxEmail
             // 
-            this.textBoxEmail.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxEmail.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.textBoxEmail.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxEmail.Location = new System.Drawing.Point(193, 209);
+            this.textBoxEmail.Location = new System.Drawing.Point(193, 216);
             this.textBoxEmail.Multiline = true;
             this.textBoxEmail.Name = "textBoxEmail";
             this.textBoxEmail.Size = new System.Drawing.Size(444, 47);
@@ -137,7 +136,7 @@
             // 
             this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxName.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxName.Location = new System.Drawing.Point(758, 206);
+            this.textBoxName.Location = new System.Drawing.Point(758, 213);
             this.textBoxName.Multiline = true;
             this.textBoxName.Name = "textBoxName";
             this.textBoxName.Size = new System.Drawing.Size(391, 47);
@@ -147,7 +146,7 @@
             // 
             this.textBoxSubject.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxSubject.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxSubject.Location = new System.Drawing.Point(241, 296);
+            this.textBoxSubject.Location = new System.Drawing.Point(241, 303);
             this.textBoxSubject.Multiline = true;
             this.textBoxSubject.Name = "textBoxSubject";
             this.textBoxSubject.Size = new System.Drawing.Size(908, 47);
@@ -157,7 +156,7 @@
             // 
             this.textBoxBody.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxBody.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxBody.Location = new System.Drawing.Point(58, 441);
+            this.textBoxBody.Location = new System.Drawing.Point(58, 448);
             this.textBoxBody.Multiline = true;
             this.textBoxBody.Name = "textBoxBody";
             this.textBoxBody.Size = new System.Drawing.Size(1091, 349);
@@ -169,7 +168,7 @@
             this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonSend.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonSend.Location = new System.Drawing.Point(719, 817);
+            this.buttonSend.Location = new System.Drawing.Point(719, 831);
             this.buttonSend.Name = "buttonSend";
             this.buttonSend.Size = new System.Drawing.Size(430, 76);
             this.buttonSend.TabIndex = 10;
@@ -184,7 +183,7 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label7.Location = new System.Drawing.Point(61, 828);
+            this.label7.Location = new System.Drawing.Point(61, 842);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(299, 56);
             this.label7.TabIndex = 11;
@@ -193,14 +192,13 @@
             // 
             // comboBox1
             // 
-            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "GMail",
             "Mail.ru"});
-            this.comboBox1.Location = new System.Drawing.Point(388, 845);
+            this.comboBox1.Location = new System.Drawing.Point(388, 859);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(271, 33);
             this.comboBox1.TabIndex = 12;
@@ -210,7 +208,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1195, 915);
+            this.ClientSize = new System.Drawing.Size(1195, 929);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.buttonSend);
@@ -224,6 +222,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(1221, 1000);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
