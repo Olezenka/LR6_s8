5c729e6a03bfbcd53b2d9daae976b07d2101706d
Admin <Admin@DESKTOP-MAHVHFA>
2026-03-02 15:49:42 +0300

    InfoGMail

---
+++ UsersClasses/InfoGMail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR5_s8.UsersClasses
+{
+    internal class InfoGMail
+    {
+    }
+}
